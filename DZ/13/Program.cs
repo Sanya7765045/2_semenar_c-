af474037a71466ed6bba78731c23c65e92981cf4
Sasha Dyachkov <sanya7765045@gmail.com>
2023-05-23 22:33:15 +0300

    2_semenar_c#

---
+++ DZ/13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Очищаем консоль
+Console.Clear();
+
+// Запрашиваем у пользователя число
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+// Проверяем, что введенное число трехзначное
+if (number >= 100)
+{
+    // Преобразуем число в строку для работы с отдельными символами
+    string strNumber = number.ToString();
+
+    // Получаем третий символ (с индексом 2) и преобразуем его в число
+    int result = int.Parse(strNumber[2].ToString());
+
+    // Выводим результат
+    Console.WriteLine($"Результат равен: {result}");
+}
+else
+{
+    // Выводим сообщение об ошибке
+    Console.WriteLine("Третьей цифры нет.");
+}
